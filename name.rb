2c36e2cba08fba889ab573244e52586a08243241
itxing123 <itxing123@163.com>
2017-10-11 15:38:01 +0800

    full_name

--- name.rb
+++ name.rb
@@ -1,4 +1,7 @@
-print '请输入您的姓名: '
-name = gets
-puts "hello #{name}"
-10.times { puts name }
+print 'your first name: '
+first_name = gets
+print 'your last name: '
+last_name = gets
+full_name = first_name + last_name
+puts "hello #{full_name}"
+10.times { puts full_name }
